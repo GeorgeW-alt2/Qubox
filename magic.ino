e01ac9774992138faefe1f9d93ef84e1b2769e9d
GeorgeW-alt2 <gw67656@gmail.com>
2024-11-28 11:34:41 +1100

    Update magic.ino

--- magic.ino
+++ magic.ino
@@ -1,5 +1,5 @@
 //Use a series of glass diodes.
-int sensorPin = A0;   // select the input pin for the potentiometer
+int sensorPin = A0;   // select the input pin for the glass diode
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
 void setup() {
