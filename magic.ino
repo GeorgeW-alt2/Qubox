74cdc578448bdc5bab8ffc62ea033e0fbf0e5083
GeorgeW-alt2 <gw67656@gmail.com>
2024-11-28 11:33:53 +1100

    Add files via upload

---
+++ magic.ino
@@ -0,0 +1,23 @@
+//Use a series of glass diodes.
+int sensorPin = A0;   // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+if (sensorValue > 0){
+Serial.println("1");
+
+}
+
+if (sensorValue == 0){
+Serial.println("0");
+
+}
+delay(100);
+}
