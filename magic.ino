56f9c4596141c1156a00634aac65b12ee1800ed7
GeorgeW-alt2 <gw67656@gmail.com>
2024-11-28 11:38:34 +1100

    Add files via upload

--- magic.ino
+++ magic.ino
@@ -1,7 +1,8 @@
+//Random number generator
 //Use a series of glass diodes.
-int sensorPin = A0;   // select the input pin for the glass diode
+int sensorPin = A0;   // select the input pin for the glass diodes
 int sensorValue = 0;  // variable to store the value coming from the sensor
-
+int n = 0;
 void setup() {
   // declare the ledPin as an OUTPUT:
   Serial.begin(9600);
@@ -11,13 +12,18 @@ void loop() {
   // read the value from the sensor:
   sensorValue = analogRead(sensorPin);
 if (sensorValue > 0){
-Serial.println("1");
+Serial.print(n);
+
+Serial.println(": 1");
 
 }
 
 if (sensorValue == 0){
-Serial.println("0");
+Serial.print(n);
+
+Serial.println(": 0");
 
 }
 delay(100);
+n++;
 }
